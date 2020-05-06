72ba0fd8860db69a12a13a3d8e0864385950b3d2
Mikko Kutilainen <mikko93@gmail.com>
2020-05-06 09:32:32 +0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    <b-form-group label="Label" description="Description">
+      <b-input />
+    </b-form-group>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
